6f0088f6f3ace80df6e773f70768bba95c03bbcb
J. P. Gaster <128156817+JP-Gaster@users.noreply.github.com>
2024-06-17 14:27:48 -0300

    Add files via upload

---
+++ aula03/repeticao.js
@@ -0,0 +1,27 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["+55(43)99956-0459", "+55(43)99924-4321"],
+};
+cliente.endereços = [
+    {
+        rua: "J. Roberto Cruz",
+        numero: 108,
+        apartamento: false,
+        casa: true,
+        complemento: "Ao lado da Unidade de Saúde 24 Horas",
+    },
+];
+cliente.endereços.push({
+    rua: "Atrol do Bikini",
+        numero: 204,
+        apartamento: false,
+        casa: false,
+        complemento: "Ao lado da Fenda do Bikini",
+})
+
+for(let chave in cliente){
+    console.log(chave)
+
+}
